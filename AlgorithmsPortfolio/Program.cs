a96778c86d60a5d99cf51d612b1be9c26681fceb
Nick Eisner <nickwe02@gmail.com>
2022-09-14 09:48:27 -0500

    Add project files.

---
+++ AlgorithmsPortfolio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmsPortfolio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
