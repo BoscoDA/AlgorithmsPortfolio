4a361f26f8fe5b6466eadd14f82ab4b2d40d8172
BoscoDA <nickwe02@gmail.com>
2022-11-04 09:37:00 -0500

    Moved location of pictures for assignment 3 ReadMe.md

--- Assignment1BigO/Program.cs
+++ Assignment1BigO/Program.cs
@@ -4,18 +4,20 @@
     {
         static void Main(string[] args)
         {
-            BigOConstantTime();
-            BigOLinearTime(40);
-            BigOQuadraticTime(50);
+            Console.WriteLine(BigOConstantTime());
+            Console.WriteLine(BigOLinearTime(40));
+            Console.WriteLine(BigOQuadraticTime(50));
         }
 
         /// <summary>
         /// This method illustrates constant time: O(1). The size of an input does not effect the time in which it will run. It will always just require one step.
         /// Constant time algorithms 
         /// </summary>
-        public static void BigOConstantTime()
+        public static string BigOConstantTime()
         {
-            Console.WriteLine("This method operates in constant time: O(1)");
+            int num;
+            num = 5 + 5;
+            return "This method operates in constant time: O(1)";
         }
 
         /// <summary>
@@ -23,27 +25,31 @@
         /// the input of the method. Meaning as the input grows, the algorithm takes proportionally longer to complete. n can represent both the size
         /// of a structure being inputted or the number itself being intputted.
         /// </summary>
-        public static void BigOLinearTime(int n)
+        public static string BigOLinearTime(int n)
         {
+            int count = 0;
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("This method runs in Linear Time: O(n)");
+                count++;
             }
+            return "This method runs in Linear Time: O(n)";
         }
 
         /// <summary>
         /// This method illustrates quadratic time because the rate at which the ouput grows is n * n (n^2) because of the nest for loops.
         /// For each time that the first loop iterates it must then go through 0..n in the inner loop causing a quadratic relationship.
         /// </summary>
-        public static void BigOQuadraticTime(int n)
+        public static string BigOQuadraticTime(int n)
         {
+            int count = 0;
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    Console.WriteLine("This method runs in Quadratic Time: O(n^2)");
+                    count++;
                 }
             }
+            return "This method runs in Quadratic Time: O(n^2)";
         }
     }
 }
