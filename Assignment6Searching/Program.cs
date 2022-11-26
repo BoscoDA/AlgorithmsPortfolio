5273d8de11309e013f1de2b7e7e1679c46dbb5f5
BoscoDA <nickwe02@gmail.com>
2022-11-25 19:31:38 -0600

    Adds assignment 6 demonstrating searching algorithms.

---
+++ Assignment6Searching/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+
+namespace Assignment6Searching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchingAlgorithms searcher = new SearchingAlgorithms();
+            Stopwatch watch = new Stopwatch();
+            int[] arr = new int[] { 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100 };
+
+            watch.Start();
+            int linearResult = searcher.LinearSearch(arr, 66);
+            watch.Stop();
+            TimeSpan linearTime = watch.Elapsed;
+            watch.Reset();
+
+            watch.Start();
+            int binaryResult = searcher.BinarySearch(arr, 66);
+            watch.Stop();
+            TimeSpan binaryTime = watch.Elapsed;
+            watch.Reset();
+
+            watch.Start();
+            int interpolationResult = searcher.InterpolationSearch(arr, 0, arr.Length - 1, 66);
+            watch.Stop();
+            TimeSpan interpolationTime = watch.Elapsed;
+            watch.Reset();
+
+            Console.WriteLine("---------------------------- Linear Search ----------------------------");
+            Console.WriteLine(@"Description: The linear search algorithm iterates through each element
+             of the data set comparing each to the target until the 
+             target is either found of the algorithm reaches the 
+             end of the data set.");
+            Console.WriteLine("\nAsymptotic Notation: Worst Case: O(n) Best Case: O(1) Average: O(n/2)\n");
+            Console.WriteLine(@"Pseudocode:
+Start LinearSearch(array, target)
+    for i = 0 to array length
+        if array[i] == target
+            return i
+        i++
+    return -1
+end LinearSearch");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("---------------------------- Binary Search ----------------------------");
+            Console.WriteLine(@"Description: The Binary search algorithm requires a sorted data set.
+             It compares the value in the middle of the data set to
+             the value being searched for. If the values are not
+             equal, the algorithm then checks to see which half
+             of the data set will contain the target. This is
+             then recursively or iteratively repeated until the
+             target is found of the algorithm reachs the end of 
+             the data set.");
+            Console.WriteLine("\nAsymptotic Notation: O(logn)\n");
+            Console.WriteLine(@"Pseudocode:
+function binary_search(A, n, T) is
+    L := 0
+    R := n − 1
+    while L ≤ R do
+        m := floor((L + R) / 2)
+        if A[m] < T then
+            L := m + 1
+        else if A[m] > T then
+            R := m − 1
+        else:
+            return m
+    return unsuccessful");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("---------------------------- Interpolation Search ----------------------------");
+            Console.WriteLine(@"Description: Interpolation Search is a improvement over Binary Search for 
+             sorted data sets that are uniformally distributed.
+             Instead of starting a the middle the data set, it
+             uses the formula for linear interpolation:
+             y = y1 + ((y2-y1)/(x2-x1))(x-x1)
+             x = target (T)
+             y = probe 
+             y1 = low index (L)
+             x1 = array[low index] (a[L])
+             y2 = high index (H)
+             x2 = array[high index] (a[H])
+             prode = a[L] + (x-a[L]/a[H]-a[L]) * (H - L)
+             ");
+            Console.WriteLine("\nAsymptotic Notation: Best Case: O(log log n) Worst Case: O(n)\n");
+            Console.WriteLine(@"Pseudocode:
+being LinearInterpolation(arr[], h, l, t
+    probe = a[L] + (x-a[L]/a[H]-a[L]) * (H - L)
+    if a[probe] = t
+        return probe
+    else if a[probe] < t
+        return LinearInterpolation(arr[], probe -1, l, t)
+    else if a[probe] > t
+        return LinearInterpolation(arr[], h, probe + 1, t)
+    return -1");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("-------------------------- Summary --------------------------");
+            Console.WriteLine("Runtimes: Linear {0}ms | Binary {1}ms | Interpolation {2}ms", linearTime.TotalMilliseconds, binaryTime.TotalMilliseconds, interpolationTime.TotalMilliseconds);
+            Console.WriteLine("Linear Search is the slowest algorithm because it has no choice but to search through each element of the dataset" +
+                "until it finds or doesn't find the target. While both Binary and Linear Interpolation searches both have ways to cut down the " +
+                "number of elements it must search through." +
+                "\n" +
+                "Binary and interpolation search both follow the same general algorithm of splitting the set into two and then checking if the value at that point is" +
+                "equal to, less, or greater than the target and adjusts its probe based off of that. The two methods vary in the way the probe index is" +
+                "calculated, binary seach always calculates it by finding the middle of the set, while the interpolation method utilizes the equation" +
+                "for linear interpolation to calculate the probe value. With this more accurate method of probing the data does need to be uniformally" +
+                "distributed for the maximum preformance increase over binary search.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment6Searching/SearchingAlgorithms.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6Searching
+{
+    public class SearchingAlgorithms
+    {
+        /// <summary>
+        /// A Binary Search function that takes in a sorted integer array and a integer Target.
+        /// Returns the index of the target if present, and -1 if it is not found. 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="T"></param>
+        /// <returns></returns>
+        public int BinarySearch(int[] arr, int T)
+        {
+            #region Pseudocode
+            //function binary_search(A, n, T) is
+            //L := 0
+            //R:= n − 1
+            //while L ≤ R do
+            //m:= floor((L + R) / 2)
+            //if A[m] < T then
+            //L := m + 1
+            //else if A[m] > T then
+            //R := m − 1
+            //else:
+            //return m
+            //return unsuccessful
+            #endregion
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while(left <= right)
+            {
+                int middle = (left + right) / 2;
+
+                if(arr[middle] < T)
+                {
+                    left = middle + 1;
+                }
+                else if(arr[middle] > T)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+
+            return -1;
+        }
+
+        public int LinearSearch(int[] arr, int t)
+        {
+            #region Pseudocode
+            //Begin
+            //1) Set i = 0
+            //2) If Li = T, go to line 4
+            //3) Increase i by 1 and go to line 2
+            //4) If i<n then return i
+            //End
+            #endregion
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == t)
+                {
+                    return i;
+                }
+            }
+            //default returns -1 because it is impossible and will denote when a element is not present in the array being searched
+            return -1;
+        }
+
+        public int InterpolationSearch(int[]arr, int low, int high, int target)
+        {
+            //Must be sorted
+            //Must have equal distribution between elements
+            //Calculates first search index by: 
+            if(low <= high && target >= arr[low] && target <= arr[high])
+            {
+                int pos = low + (((high - low) / (arr[high] - arr[low])) * (target - arr[low]));
+
+                if (arr[pos] == target)
+                {
+                    return pos;
+                }
+
+                else if (arr[pos] < target)
+                {
+                    return InterpolationSearch(arr, pos + 1, high, target);
+                }
+
+                else if (arr[pos] > target)
+                {
+                    return InterpolationSearch(arr, low, pos - 1, target);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
