60acb9e1356842c015709912016c3318dcb2a507
BoscoDA <nickwe02@gmail.com>
2022-12-06 15:45:36 -0600

    Added binary tree and binary search tree classes

--- Assignment7Trees/Program.cs
+++ Assignment7Trees/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Assignment7Trees
+﻿using Assignment5Sorting;
+
+namespace Assignment7Trees
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] vals = Utilities.ReadFromTextFile("../../../scores.txt");
+            SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+            sortingAlgorithms.QuickSort(vals, 0, vals.Length - 1);
+
+            BinarySearchTree tree = new BinarySearchTree();
+            foreach (int i in vals)
+            {
+                tree.Root = tree.Insert(tree.Root, i);
+            }
+
+            tree.PreOrderTraversal(tree.Root);
+
+            Console.WriteLine("\n\n");
+
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.BuildTreeFromArray(vals,0);
+
+            binaryTree.PreorderPrint(binaryTree.Root);
+            Console.ReadKey();
         }
     }
 }
---
+++ Assignment7Trees/node.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7Trees
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node? Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public BinaryTree(int value)
+        {
+            Root = new Node(value);
+        }
+
+        public void BuildTreeFromArray(int[] arr, int i = 0)
+        {
+            Root = buildTreeFromArray(arr, i);
+        }
+
+        private Node buildTreeFromArray(int[] arr, int i = 0) 
+        {
+            Node? root = null;
+
+            if (i < arr.Length)
+            {
+                root = new Node(arr[i]);
+                root.Left = buildTreeFromArray(arr, 2 * i + 1);
+                root.Right = buildTreeFromArray(arr, 2 * i + 2);
+            }
+
+            return root!;
+        }
+
+        public void PreorderPrint(Node? root)
+        {
+            if (root != null)
+
+            {
+                Console.Write(root.Value + " ");
+
+                PreorderPrint(root.Left);
+
+                PreorderPrint(root.Right);
+
+            }
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        public Node? Root;
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        public BinarySearchTree(int value)
+        {
+            Root = new Node(value);
+        }
+
+        public Node Insert(Node root, int value)
+        {
+            if (root == null)
+            {
+                return new Node(value);
+            }
+            Node? currentNode = root;
+            if(value <= currentNode.Value)
+            {
+                currentNode.Left = Insert(currentNode.Left!, value);
+            }
+            else if(value >= currentNode.Value)
+            {
+                currentNode.Right = Insert(currentNode.Right!, value);
+            }
+            return root;
+        }
+
+
+        public void Remove()
+        {
+
+        }
+
+
+
+        public void PreOrderTraversal(Node? root)
+        {
+            if (root != null)
+            {
+                Console.Write(root.Value + " ");
+                PreOrderTraversal(root.Left);
+                PreOrderTraversal(root.Right);
+            }
+        }
+
+        public void InOrderTraversal(Node? root)
+        {
+            if (root != null)
+            {
+                InOrderTraversal(root.Left);
+                Console.Write(root.Value + " ");
+                InOrderTraversal(root.Right);
+            }
+        }
+
+        public void PostOrderTraversal(Node? root)
+        {
+            if (root != null)
+            {
+                PostOrderTraversal(root.Left);
+                PostOrderTraversal(root.Right);
+                Console.Write(root.Value + " ");
+            }
+        }
+
+    }
+}
